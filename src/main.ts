43baf3ca99a611e2202b2e7f556db7cedcc8e83f
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-10-31 13:02:27 +0200

    Set up main.ts with Firebase initialisation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { firebaseConfig } from '../firebase.config';
+import { initializeApp } from 'firebase/app';
+
+
+initializeApp(firebaseConfig);
+
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
