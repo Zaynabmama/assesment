4bec9b4fdee30ced0649adc2f912a4015c6c01b8
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-11-01 13:42:27 +0200

    update signup component, and modify styles

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,28 +1,50 @@
 import { Component } from '@angular/core';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import { FormsModule } from '@angular/forms';
+import { ButtonComponent } from "../components/button/button.component";
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css'],
   standalone: true,
-  imports: [FormsModule]
+  imports: [FormsModule, ButtonComponent,RouterModule ,CommonModule]
 })
 export class SignupComponent {
+  
+  
   email: string = '';
   password: string = '';
+  errorMessage: string = ''; 
+  constructor(private router: Router) {} 
+
+
+  // isPasswordValid(): boolean {
+  //   const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+  //   return passwordRegex.test(this.password);
+  // }
+  
 
   onSubmit() {
     const auth = getAuth();
     createUserWithEmailAndPassword(auth, this.email, this.password)
-      .then((userCredential) => {
+      .then(async(userCredential) => {
         const user = userCredential.user;
         console.log('Signed up user:', user);
+        this.errorMessage = '';
+        this.router.navigate(['/home']);
       })
       .catch((error) => {
-        const errorMessage = error.message;
-        console.error('Error signing up:', errorMessage);
+        this.handleError(error);
       });
+    }
+
+  private handleError(error: any) {
+    
+    this.errorMessage = 'An error occurred during sign-up. Please try again later.';
+  
   }
 }
