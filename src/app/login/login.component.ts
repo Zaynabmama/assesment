b4bc9233bb8ad49492c664533bac0c7d45cc991e
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-11-01 14:39:51 +0200

    handle login with session

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,28 +1,55 @@
 import { Component } from '@angular/core';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { FormsModule } from '@angular/forms';
+import { ButtonComponent } from "../components/button/button.component";
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
   standalone: true,
-  imports: [FormsModule]
+  imports: [FormsModule, ButtonComponent,RouterModule,CommonModule]
 })
 export class LoginComponent {
   email: string = '';
   password: string = '';
+  errorMessage: string | null = null;
+  constructor(private router: Router) {} 
 
   onSubmit() {
     const auth = getAuth();
+    const db = getFirestore();
     signInWithEmailAndPassword(auth, this.email, this.password)
-      .then((userCredential) => {
+      .then(async(userCredential) => {
         const user = userCredential.user;
-        console.log(user);
+        const userDoc = await getDoc(doc(db, "users", user.uid));
+        if (userDoc.exists()) {
+          const userData = userDoc.data();
+          const userRole = userData['role'];//gett role from firebose
+          sessionStorage.setItem('userEmail', this.email);
+          sessionStorage.setItem('userRole', userRole);
+
+        this.errorMessage = null; 
+        //this.router.navigate(['/home']);
+        if (userRole === 'admin') {
+          this.router.navigate(['/admin']);
+        } else {
+          this.router.navigate(['/home']);
+        }
+        }else {
+          this.errorMessage = 'User not found in Firestore. Please contact support.';
+        }
       })
       .catch((error) => {
-        const errorMessage = error.message;
-        console.error(errorMessage);
+        this.handleError(error);
       });
   }
+  private handleError(error: any) {
+
+    this.errorMessage = 'An error occurred during sign-up. Please try again later.';
+    
+    }
 }
