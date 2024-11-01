11a7050a83dfb80f8455866f5ec8e8c4b82b9637
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-11-01 14:39:26 +0200

    define routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'home', component: NavbarComponent },//will change it later
+  { path: 'admin', component: NavbarComponent },//will change it later
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
