0d887253d1b74c861ed91ded0d990043f8415eaf
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-11-01 15:06:05 +0200

    use adminguard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,14 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { authGuard } from './guards/auth.guard';
+import { adminGuard, authGuard } from './guards/auth.guard';
 
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'home', component: NavbarComponent , canActivate: [authGuard] },//will change it later
-  { path: 'admin', component: NavbarComponent },//will change it later
+  { path: 'admin', component: NavbarComponent ,canActivate: [adminGuard]  },//will change it later
+ 
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
