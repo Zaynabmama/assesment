dc3e59783cbec55d3c948947e0bd200d3d1614f5
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-11-01 14:55:17 +0200

    check for authenticated user

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,19 @@
 import { CanActivateFn } from '@angular/router';
+import { getAuth } from 'firebase/auth';
+import { Router } from '@angular/router';
+import { inject } from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  const auth = getAuth();
+  const user = auth.currentUser; 
+  const router = inject(Router); 
+  
+  if (user) {
   return true;
+} else {
+
+  router.navigate(['/login']);
+  return false;
+}
+
 };
