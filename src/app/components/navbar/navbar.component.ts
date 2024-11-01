8ba57880f8fdae70d7d0f5293e1089eb6a82f22e
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-11-01 15:20:09 +0200

    add signOut function from Firebase Authentication <> remove the user information from sessionStorage

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { getAuth, signOut } from 'firebase/auth';
 @Component({
   selector: 'app-navbar',
   standalone: true,
@@ -9,11 +10,21 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent {
   constructor(private router: Router) {}
-
+  
   logout() {
+    const auth = getAuth();
+    signOut(auth) 
+      .then(() => {
+    
+        sessionStorage.removeItem('userEmail');
+        sessionStorage.removeItem('userRole');
     
     this.router.navigate(['/login']); 
-  }
+  })
+  .catch((error) => {
+    console.error( error);
+  });
+}
   navigateToCart() {
     this.router.navigate(['/cart']);
   }
