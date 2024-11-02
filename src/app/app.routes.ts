12803bab0cc3037861c01ba5ce71660b15d5578c
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-11-02 13:31:14 +0200

    update routes for single product page and Home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,8 @@ import { SignupComponent } from './signup/signup.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { adminGuard, authGuard } from './guards/auth.guard';
 import { HomeComponent } from './components/home/home.component';
-import { ProductCatalogComponent } from './components/product-catalog/product-catalog.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+// import { ProductCatalogComponent } from './components/product-catalog/product-catalog.component';
 
 
 export const routes: Routes = [
@@ -12,6 +13,6 @@ export const routes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'home', component: HomeComponent , canActivate: [authGuard] },
   { path: 'admin', component: NavbarComponent ,canActivate: [adminGuard]  },//will change it later
- 
+  { path: 'product/:id', component: ProductDetailComponent ,canActivate: [authGuard] },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
