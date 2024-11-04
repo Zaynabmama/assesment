514896611d2d20b3a6a9dd535af2409c855111b0
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-11-04 16:46:10 +0200

    Add weather feature to navbar and update CSS for responsive design

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,16 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, computed } from '@angular/core';
 import { Router } from '@angular/router';
 import { getAuth, signOut } from 'firebase/auth';
+
+import { WeatherService } from '../../services/weather.service';
+import { CommonModule } from '@angular/common';
+
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
-  constructor(private router: Router) {}
+export class NavbarComponent implements OnInit{
+  weatherIcon = computed(() => {
+    const weather = this.weatherService.currentWeather();
+    return weather ? this.getWeatherIcon(weather.weathercode) : undefined;
+  });
+
+  temperature = computed(() => {
+    const weather = this.weatherService.currentWeather();
+    return weather ? weather.temperature : undefined;
+  });
+
+  constructor(private router: Router,  private weatherService: WeatherService) {}
   
+
+  ngOnInit() {
+    const latitude = 33.888630;
+    const longitude = 35.495480;
+    this.weatherService.getWeather(latitude, longitude);
+  }
+
+  getWeatherIcon(weatherCode: number): string {
+    switch (weatherCode) {
+      case 0: return 'fas fa-sun';
+      case 1: case 2: case 3: return 'fas fa-cloud-sun';
+      case 45: case 48: return 'fas fa-smog';
+      case 61: case 63: case 65: return 'fas fa-cloud-showers-heavy';
+      default: return 'fas fa-cloud'; 
+    }
+  }
   logout() {
     const auth = getAuth();
     signOut(auth) 
