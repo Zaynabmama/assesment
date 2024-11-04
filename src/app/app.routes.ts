0bd01ea054f362dc51b95d3dde3df22f6ef0a441
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-11-04 16:43:05 +0200

    define routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { adminGuard, authGuard } from './guards/auth.guard';
-import { HomeComponent } from './components/home/home.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+
 // import { ProductCatalogComponent } from './components/product-catalog/product-catalog.component';
 
 
